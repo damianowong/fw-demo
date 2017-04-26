af2819dced3bbbcd59533caf0bcc00ff90039d80
damianowong <damiano.wong@alchemysystems.com>
2017-04-26 15:23:58 -0400

    chinese

---
+++ chineseHello.js
@@ -0,0 +1,11 @@
+var lang = window.navigator.language;
+//changes
+if(language == 'en') {
+ console.log('Hello');
+}
+else if (language == 'es') {
+ console.log("Ola");
+}
+else {
+ console.log('Bonjour');
+}
